4b12d804506a93fc65e97edd44011259a967cc5d
jzi <jzi@sitecore.net>
2025-07-30 09:24:11 -0400

    Added Shadcn ui components Added FlightPicker component

--- skywings/.sitecore/component-map.ts
+++ skywings/.sitecore/component-map.ts
@@ -4,9 +4,17 @@ import { Form } from '@sitecore-content-sdk/nextjs';
 // end of built-in components
 
 // Components imported from the app itself
+import * as select from 'src/components/ui/select';
+import * as popover from 'src/components/ui/popover';
+import * as input from 'src/components/ui/input';
+import * as card from 'src/components/ui/card';
+import * as calendar from 'src/components/ui/calendar';
+import * as button from 'src/components/ui/button';
 import * as PartialDesignDynamicPlaceholder from 'src/components/partial-design-dynamic-placeholder/PartialDesignDynamicPlaceholder';
 import * as Header from 'src/components/navigation/Header';
 import * as Footer from 'src/components/navigation/Footer';
+import * as utils from 'src/components/lib/utils';
+import * as FlightPicker from 'src/components/banners/FlightPicker';
 
 
 // Components must be registered within the map to match the string key with component name in Sitecore
@@ -14,9 +22,17 @@ export const componentMap = new Map<string, NextjsContentSdkComponent>([
   ['BYOCWrapper', BYOCWrapper],
   ['FEaaSWrapper', FEaaSWrapper],
   ['Form', Form],
+  ['select', select],
+  ['popover', popover],
+  ['input', input],
+  ['card', card],
+  ['calendar', calendar],
+  ['button', button],
   ['PartialDesignDynamicPlaceholder', PartialDesignDynamicPlaceholder],
   ['Header', Header],
   ['Footer', Footer],
+  ['utils', utils],
+  ['FlightPicker', FlightPicker],
 ]);
 
 export default componentMap;

c61aa8b9ce5edccb4806f81028340ea14e807de3
jzi <jzi@sitecore.net>
2025-07-30 13:04:33 -0400

    Added SkywingsServices component

--- travel-hospitality/.sitecore/component-map.ts
+++ travel-hospitality/.sitecore/component-map.ts
@@ -10,12 +10,14 @@ import * as input from 'src/components/ui/input';
 import * as card from 'src/components/ui/card';
 import * as calendar from 'src/components/ui/calendar';
 import * as button from 'src/components/ui/button';
+import * as badge from 'src/components/ui/badge';
 import * as FlightPicker from 'src/components/pickers/FlightPicker';
 import * as PartialDesignDynamicPlaceholder from 'src/components/partial-design-dynamic-placeholder/PartialDesignDynamicPlaceholder';
+import * as SkywingsServices from 'src/components/page-content/SkywingsServices';
 import * as Header from 'src/components/navigation/Header';
 import * as Footer from 'src/components/navigation/Footer';
 import * as utils from 'src/components/lib/utils';
-import * as PopularDestinations from '@/components/destinations/PopularDestinations';
+import * as PopularDestinations from 'src/components/destinations/PopularDestinations';
 
 
 // Components must be registered within the map to match the string key with component name in Sitecore
@@ -29,8 +31,10 @@ export const componentMap = new Map<string, NextjsContentSdkComponent>([
   ['card', card],
   ['calendar', calendar],
   ['button', button],
+  ['badge', badge],
   ['FlightPicker', FlightPicker],
   ['PartialDesignDynamicPlaceholder', PartialDesignDynamicPlaceholder],
+  ['SkywingsServices', SkywingsServices],
   ['Header', Header],
   ['Footer', Footer],
   ['utils', utils],

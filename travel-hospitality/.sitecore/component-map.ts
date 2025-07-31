f15a9ab9170426fb22644224b8a959ffaeae11e5
jzi <jzi@sitecore.net>
2025-07-31 12:37:50 -0400

    Added destination detail component

--- travel-hospitality/.sitecore/component-map.ts
+++ travel-hospitality/.sitecore/component-map.ts
@@ -4,6 +4,8 @@ import { Form } from '@sitecore-content-sdk/nextjs';
 // end of built-in components
 
 // Components imported from the app itself
+import * as tabs from 'src/components/ui/tabs';
+import * as separator from 'src/components/ui/separator';
 import * as select from 'src/components/ui/select';
 import * as popover from 'src/components/ui/popover';
 import * as input from 'src/components/ui/input';
@@ -18,7 +20,7 @@ import * as PartialDesignDynamicPlaceholder from 'src/components/partial-design-
 import * as SkywingsServices from 'src/components/page-content/SkywingsServices';
 import * as SkywingsFleet from 'src/components/page-content/SkywingsFleet';
 import * as OfferBlock from 'src/components/page-content/OfferBlock';
-import * as BlogDetail from 'src/components/destinations/FeaturedDestinations'
+import * as BlogDetail from 'src/components/page-content/BlogDetail';
 import * as Author from 'src/components/page-content/Author';
 import * as Header from 'src/components/navigation/Header';
 import * as Footer from 'src/components/navigation/Footer';
@@ -27,6 +29,7 @@ import * as AllBlogs from 'src/components/listers/AllBlogs';
 import * as utils from 'src/components/lib/utils';
 import * as PopularDestinations from 'src/components/destinations/PopularDestinations';
 import * as FeaturedDestinations from 'src/components/destinations/FeaturedDestinations';
+import * as DestinationDetail from 'src/components/destinations/DestinationDetail';
 
 
 // Components must be registered within the map to match the string key with component name in Sitecore
@@ -34,6 +37,8 @@ export const componentMap = new Map<string, NextjsContentSdkComponent>([
   ['BYOCWrapper', BYOCWrapper],
   ['FEaaSWrapper', FEaaSWrapper],
   ['Form', Form],
+  ['tabs', tabs],
+  ['separator', separator],
   ['select', select],
   ['popover', popover],
   ['input', input],
@@ -57,6 +62,7 @@ export const componentMap = new Map<string, NextjsContentSdkComponent>([
   ['utils', utils],
   ['PopularDestinations', PopularDestinations],
   ['FeaturedDestinations', FeaturedDestinations],
+  ['DestinationDetail', DestinationDetail],
 ]);
 
 export default componentMap;
